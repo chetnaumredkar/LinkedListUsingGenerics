24bdc481c1d4c38a7e80ed13cc27fe1d77edb1ed
kolhechetna <kolhe.chetna@gmail.com>
2022-12-01 13:37:29 +0530

    Added:Add method in Linked List

--- src/com/bridgelabz/LinkedListCustom.java
+++ src/com/bridgelabz/LinkedListCustom.java
@@ -56,19 +56,7 @@ package com.bridgelabz;
 		}
 		
 		
-		/*public void append(INode newNode) {
-			if(this.tail == null) {
-				this.tail = newNode;
-			}
-			if(this.head==null) {
-				this.head=newNode;
-			}
-				else {
-				INode tempNode = this.tail;
-				this.tail.setNext(tempNode);
-				this.tail = newNode;
-				}}
-		*/
+		
 		public void dispalyLinkedList(){
 			System.out.println("My list"+head);
 		}
